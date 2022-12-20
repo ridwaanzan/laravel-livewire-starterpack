f3d6d2d550ca24f1136cccb72b262d46942e8e92
ridwan zan haqqi <ridwan.haqi@gmail.com>
2022-12-21 04:41:07 +0700

    persiapan pada frontend

--- app/Http/Livewire/Front/Auth/Register.php
+++ app/Http/Livewire/Front/Auth/Register.php
@@ -6,6 +6,15 @@ use Livewire\Component;
 
 class Register extends Component
 {
+    public $name, $email, $password, $password_confirmation;
+    public $place_of_birth, $date_of_birth, $province_id;
+    public $regency_id, $district_id;
+
+    public function register()
+    {
+        // code
+    }
+
     public function render()
     {
         return view('livewire.front.auth.register');
